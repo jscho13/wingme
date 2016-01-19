14be326299803014ed920490aa8f0a6dff4e8f69
Joseph Cho <jscho13@gmail.com>
2016-01-19 15:30:54 -0500

    hound 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-          :registerable,
-          :recoverable,
-          :rememberable,
-          :trackable,
-          :validatable
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   def self.match_list(user_id)
     matches = []
