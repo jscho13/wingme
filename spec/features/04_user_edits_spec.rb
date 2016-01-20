437fef8b31649337e880264fbf221d9c920d0a54
Joseph Cho <jscho13@gmail.com>
2016-01-20 14:06:38 -0500

    add edit_user functionality and tests

---
+++ spec/features/04_user_edits_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature "user edits their profile page" do
+  let!(:user_1) { User.create(
+    email: 'jscho13@gmail.com',
+    first_name: 'Joseph',
+    last_name: 'Cho',
+    gender: 'Male',
+    password: 'Applied91')
+  }
+
+  scenario "unauthenticated user visits edit page" do
+    visit '/users/edit'
+    expect(page).to have_content('You need to sign in or sign up')
+  end
+
+  scenario "user succesfully edits their name" do
+    sign_in_as(user_1)
+    visit '/users/edit'
+    choose 'user_gender_other'
+    fill_in 'Current password', with: user_1.password
+    click_on "Update"
+    expect(page).to have_content('Your account has been updated successfully.')
+  end
+
+  scenario "user cannot edit with invalid confirmation" do
+    sign_in_as(user_1)
+    visit '/users/edit'
+    fill_in 'Current password', with: "abcd123"
+    click_on "Update"
+    expect(page).to have_content('Current password is invalid')
+  end
+end
