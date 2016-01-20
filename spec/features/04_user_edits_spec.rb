f516185e1b56ef5d121c85ccd1ed9b45321a0db5
Joseph Cho <jscho13@gmail.com>
2016-01-20 14:11:12 -0500

    coveralls complete

--- spec/features/04_user_edits_spec.rb
+++ spec/features/04_user_edits_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 feature "user edits their profile page" do
-  let!(:user_1) { User.create(
-    email: 'jscho13@gmail.com',
-    first_name: 'Joseph',
-    last_name: 'Cho',
-    gender: 'Male',
-    password: 'Applied91')
-  }
+  let!(:user_1) do
+    User.create(
+      email: 'jscho13@gmail.com',
+      first_name: 'Joseph',
+      last_name: 'Cho',
+      gender: 'Male',
+      password: 'Applied91'
+    )
+  end
 
   scenario "unauthenticated user visits edit page" do
     visit '/users/edit'
