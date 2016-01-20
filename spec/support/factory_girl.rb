aa696a08fc05356c3f91828c7250c2d4cca60583
Joseph Cho <jscho13@gmail.com>
2016-01-20 13:22:05 -0500

    hound 2

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    sequence :email { |n| "jscho#{n}@gmail.com" }
+    sequence(:email) { |n| "jscho#{n}@gmail.com" }
     first_name "Joseph"
     last_name "Cho"
     gender "Male"
-    sequence :password { |n| "Applied#{n}" }
+    sequence (:password) { |n| "Applied#{n}" }
   end
 end
