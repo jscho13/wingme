357fde26811a6ae54774b1ee5a0027dfa704cfc2
Joseph Cho <jscho13@gmail.com>
2016-01-25 14:53:00 -0500

    finish fonts and forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomeController < ApplicationController; layout false
   # class HomeController < ApplicationController; layout false
   # is an optional class that makes prevents the application controller
   def index
