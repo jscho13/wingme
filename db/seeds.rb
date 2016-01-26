2ea72e4dcc1a02828134cd11faa9545358d76f21
Joseph Cho <jscho13@gmail.com>
2016-01-25 20:34:49 -0500

    finish all pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,70 +8,18 @@ require 'faker'
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+20.times do
+  User.create(
+    email: Faker::Email,
+    name: Faker::Name,
+    gender: 'Male',
+    password: '12341234'
+  )
+end
+
 User.create(
   email: 'jscho13@gmail.com',
-  first_name: 'Joseph',
-  last_name: 'Cho',
+  name: 'Joseph Cho',
   gender: 'Male',
   password: '12341234'
-)
-
-User.create(
-  email: 'krdixon83@gmail.com',
-  first_name: 'Dickson',
-  last_name: 'Lau',
-  gender: 'Male',
-  password: '12341234'
-)
-
-User.create(
-  email: 'shinshiny@gmail.com',
-  first_name: 'Hannah',
-  last_name: 'Shin',
-  gender: 'Female',
-  password: '12341234'
-)
-
-User.create(
-  email: 'jrpespisa@verizon.net',
-  first_name: 'John',
-  last_name: 'Pespisa',
-  gender: 'Male',
-  password: '12341234'
-)
-
-User.create(
-  email: 'amywernerallen@arcadiasolutions.com',
-  first_name: 'Amy',
-  last_name: 'Werner',
-  gender: 'Female',
-  password: '12341234'
-)
-
-User.create(
-  email: 'leonrusso@gmail.com',
-  first_name: 'Leon',
-  last_name: 'Russo',
-  gender: 'Male',
-  password: '12341234'
-)
-
-User.create(
-  email: 'jujukoo@gmail.com',
-  first_name: 'Julie',
-  last_name: 'Gendrano',
-  gender: 'Female',
-  password: '12341234'
-)
-
-UserFriend.create(
-  user_id: 1,
-  friend_id: 2,
-  pending: false
-)
-
-UserFriend.create(
-  user_id: 2,
-  friend_id: 1,
-  pending: false
 )
